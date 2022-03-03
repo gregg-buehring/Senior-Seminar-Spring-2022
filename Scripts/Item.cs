68fe76f7452d78bbdb76f3f51ab819a3241dc625
gregg-buehring <greggbuehring@gmail.com>
2022-03-03 11:58:30 -0500

    Create Item.cs

---
+++ Scripts/Item.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+void OnTriggerEnter(Collider collision){
+        if(collision.gameObject.tag == "Player"){
+            gameObject.SetActive(false);
+        }
+    }
+}
