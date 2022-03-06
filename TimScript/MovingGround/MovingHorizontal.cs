a02f7e1eff321c558f0dec6d54bf66a20adcd094
Thuong Pham <75382025+timphamvn33@users.noreply.github.com>
2022-03-06 18:34:28 -0500

    Add files via upload

---
+++ TimScript/MovingGround/MovingHorizontal.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingHorizontal : MonoBehaviour
+{
+    Player_Movement3D playerClass;
+    public float amp;
+    public float speed;
+    public float xStart;
+    public GameObject player;
+    private float dist;
+    public float yPosition;
+    public float zPosition;
+    public bool isGround;
+    public bool isCube;
+    public bool moveLeft;
+    public bool isNotMove;
+    public float yStart;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+         playerClass = GameObject.FindGameObjectWithTag("Player").GetComponent<Player_Movement3D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SawMove();
+        SawCollision();
+    }
+    // move the saw up and down
+    void SawMove(){
+        float xPosition;
+        if(isNotMove){
+            xPosition=xStart;
+            yPosition=yStart+Mathf.Sin(Time.time*speed)*amp;
+        }
+        else{
+            if(moveLeft){
+                xPosition = xStart-Mathf.Sin(Time.time*speed)*amp;
+            }
+            else{
+                xPosition = xStart+Mathf.Sin(Time.time*speed)*amp;
+            }
+        }
+        
+
+        transform.position = new Vector3(xPosition,yPosition , zPosition);
+    }
+   
+    
+    // check if the player colide the saw and die
+     void SawCollision(){
+            dist = Vector3.Distance(transform.position, player.transform.position);
+            // check if the object is 'ground' 'saw' or 'spider' 
+            
+            if(!isGround){
+                if(dist<100){
+                    player.SetActive(false);
+                }
+            }
+            else if(isCube){
+                if(dist<200){
+                   
+                    playerClass.FollowGroundCube();
+                }
+            }
+      }
+}
