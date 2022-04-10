b436a32929d0c6848c8442aa07c37fafe096a610
Thuong Pham <75382025+timphamvn33@users.noreply.github.com>
2022-04-10 16:29:09 -0400

    Update GamePlayManager.cs

--- GamePlayManager.cs
+++ GamePlayManager.cs
@@ -8,9 +8,11 @@ public class GamePlayManager : MonoBehaviour
     [SerializeField]
     private Text Time;
     private Image Clock;
+    private Text GameOverText;
     private GameObject player;
     public int countdownTimer;
     private bool startTime=false;
+    private int countTouch=0;
     
 
     // Start is called before the first frame update
@@ -20,44 +22,43 @@ public class GamePlayManager : MonoBehaviour
     }
     void Start()
     {
-       
        Time= GameObject.Find("Time").GetComponent<Text>();
        Clock = GameObject.Find("Clock").GetComponent<Image>();
+       GameOverText=GameObject.Find("GameOverText").GetComponent<Text>();
+       GameOverText.enabled=false;
        Clock.enabled=false;
        Time.enabled=false;
     }
     void Update(){
-       
-         if(startTime){
+       if(countTouch==1){
+           if(startTime){
             Time.text=countdownTimer.ToString();
             StartCoroutine("CountDown");
             startTime=false;
-        }
+           }
+       }   
     }
     IEnumerator CountDown(){
-
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(1f);// count down per second 
         countdownTimer-=1;
-
-        Time.text=countdownTimer.ToString();
+        Time.text=countdownTimer.ToString();// update the timer into the UI 
         StartCoroutine("CountDown");
         if(countdownTimer<=0){
             StopCoroutine("CountDown");
+            // Timer disappear
             Time.enabled=false;
             Clock.enabled=false;
-
-        }
-        
+            GameOverText.enabled=true;// show the "Game Over" text. 
+        }   
     }
-
       // touch the bottom ground and triger the time
      void OnCollisionEnter(Collision collision){
         if(collision.gameObject.tag == "BottomGround"){
             startTime=true;
+            countTouch++; // count how many time the player touch the ground. 
+            // show the Timer
             Time.enabled=true;
             Clock.enabled=true;
-            
         }
     }
-
 }
